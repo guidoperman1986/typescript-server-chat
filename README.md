6755c2e4c486c369d3cc97dceba33ed3be241815
Guido Perman <guidoperman@gmail.com>
2019-12-09 11:23:17 -0800

    Primer commit REST server

---
+++ README.md
@@ -0,0 +1,17 @@
+#Socket-Server
+
+Reconstruir modulos de node
+```
+npm install
+```
+
+Generar el DIST
+```
+tsc -w
+```
+
+Levantar el servidor, cualquiera de estos 2 comandos
+```
+nodemon dist/
+node dist/
+```
---
+++ routes/router.ts
@@ -0,0 +1,26 @@
+import { Router, Request, Response } from 'express';
+
+export const router = Router();
+
+router.get('/mensajes',(req:Request,res:Response)=>{
+
+    res.json({
+        ok:true,
+        mensaje:'todo ok'
+    })
+})
+
+
+router.post('/mensajes',(req:Request,res:Response)=>{
+    const cuerpo = req.body.cuerpo
+    const de = req.body.de
+    const id = req.params.id
+
+
+    res.json({
+        ok:true,
+        mensaje:'todo ok POST'
+    })
+})
+
+
